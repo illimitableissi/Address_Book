dcc76a6fd0e5de44677e8bd562051754ab4a1af4
SOVEREIGN <isimemeni@gmail.com>
2021-09-04 14:56:31 -0400

    business cards display data

--- scripts.js
+++ scripts.js
@@ -7,21 +7,7 @@ $http({
     url: '/ab.xml'
     }).then(function success(response) {
         var json = xmlToJson.parse(response.data);
-        console.log(json.AddressBook.Contact)
         $scope.contacts = json.AddressBook.Contact
-     
-        // contacts.map(contact => {
-        //     $scope.company = contact.CompanyName;
-        //     $scope.contactName = contact.ContactName;
-        //     $scope.contactTitle = contact.ContactTitle;
-        //     $scope.address = contact.Address;
-        //     $scope.city = contact.City;
-        //     $scope.email = contact.Email;
-        //     $scope.postalCode = contact.PostalCode;
-        //     $scope.country = contact.Country;
-        //     $scope.phone = contact.Phone;
-        //     $scope.fax = contact.Fax
-        // })
         },
     );
 });
